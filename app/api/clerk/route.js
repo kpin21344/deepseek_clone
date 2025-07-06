4c0d370eda46a713b7079afb39aa7ea42ee5fbf0
kpin21344 <kpin21344@gmail.com>
2025-07-07 01:06:36 +0530

    first commit

---
+++ app/api/clerk/route.js
@@ -0,0 +1,40 @@
+import { Webhook } from "svix";
+import connectDB from "@/config/db";
+import User from "@/models/User";
+import { headers } from "next/headers";
+import { NextRequest } from "next/server";
+
+export async function POST(req){
+    const wh = new Webhook(process.env.SIGNING_SECRET)
+    const headerPayload=await headers()
+    const svixHeaders={
+        'svix-id':headerPayload.get('svix-id'),
+        'svix-signature':headerPayload.get('svix-signature'),
+    };
+    //get payload and verify it
+    const payload=await req.json();
+    const body= JSON.stringify(payload)
+    // prep the user data to be saved
+    const userData={
+        _id:data.id,
+        email:data.email_address[0].email_address,
+        name:`${data.first_name} ${data.last_name}`,
+        image:data.image_url,
+    };
+    await connectDB;
+    switch (type) {
+        case 'user.created':
+            await  User.create(userData)
+            break;
+        case 'user.updated':
+            await  User.findByIdAndUpdate(data.id,userData)
+            break;
+        case 'user.deleted':
+            await  User.findByIdAndDelete(data.id)
+            break;        
+    
+        default:
+            break;
+    }
+    return NextRequest.json({message:'event recieved'})
+}
