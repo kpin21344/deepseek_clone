f5fc8f39031bdd5a80c958711932b4463754a72a
kpin21344 <kpin21344@gmail.com>
2025-07-07 02:04:35 +0530

    modified:   app/api/clerk/route.js

--- app/api/clerk/route.js
+++ app/api/clerk/route.js
@@ -19,7 +19,7 @@ export async function POST(req){
     // prep the user data to be saved
     const userData={
         _id:data.id,
-        email:data.email_address[0].email_address,
+        email:data.email_addresses[0].email_addresses,
         name:`${data.first_name} ${data.last_name}`,
         image:data.image_url,
     };
